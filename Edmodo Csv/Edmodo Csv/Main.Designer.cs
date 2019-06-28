9576fdb3fb5d7f90660fd59061e454bb8fb69d8d
Jeff94sl <jeffrynsl@hotmail.es>
2019-06-28 08:57:38 -0600

    v1

---
+++ Edmodo Csv/Edmodo Csv/Main.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Edmodo_Csv
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.Menus = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AbrirCSV = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acerdaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Abrir = new System.Windows.Forms.OpenFileDialog();
+            this.Guardar = new System.Windows.Forms.SaveFileDialog();
+            this.DGV = new System.Windows.Forms.DataGridView();
+            this.Menus.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Menus
+            // 
+            this.Menus.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.Menus.Location = new System.Drawing.Point(0, 0);
+            this.Menus.Name = "Menus";
+            this.Menus.Size = new System.Drawing.Size(687, 24);
+            this.Menus.TabIndex = 0;
+            this.Menus.Text = "Menu";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AbrirCSV,
+            this.exportarToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // AbrirCSV
+            // 
+            this.AbrirCSV.Name = "AbrirCSV";
+            this.AbrirCSV.Size = new System.Drawing.Size(122, 22);
+            this.AbrirCSV.Text = "Abrir Csv";
+            this.AbrirCSV.Click += new System.EventHandler(this.AbrirCSV_Click);
+            // 
+            // exportarToolStripMenuItem
+            // 
+            this.exportarToolStripMenuItem.Name = "exportarToolStripMenuItem";
+            this.exportarToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.exportarToolStripMenuItem.Text = "Exportar";
+            this.exportarToolStripMenuItem.Click += new System.EventHandler(this.exportarToolStripMenuItem_Click);
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acerdaDeToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // acerdaDeToolStripMenuItem
+            // 
+            this.acerdaDeToolStripMenuItem.Name = "acerdaDeToolStripMenuItem";
+            this.acerdaDeToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.acerdaDeToolStripMenuItem.Text = "Acerda de";
+            this.acerdaDeToolStripMenuItem.Click += new System.EventHandler(this.acerdaDeToolStripMenuItem_Click);
+            // 
+            // Abrir
+            // 
+            this.Abrir.Filter = "csv|*.csv";
+            // 
+            // Guardar
+            // 
+            this.Guardar.Filter = "Excel|*.xlsx";
+            // 
+            // DGV
+            // 
+            this.DGV.AllowUserToAddRows = false;
+            this.DGV.AllowUserToDeleteRows = false;
+            this.DGV.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DGV.Location = new System.Drawing.Point(0, 24);
+            this.DGV.Name = "DGV";
+            this.DGV.ReadOnly = true;
+            this.DGV.Size = new System.Drawing.Size(687, 345);
+            this.DGV.TabIndex = 3;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 369);
+            this.Controls.Add(this.DGV);
+            this.Controls.Add(this.Menus);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.Menus;
+            this.Name = "Main";
+            this.Text = "EdmodoCSV";
+            this.Menus.ResumeLayout(false);
+            this.Menus.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip Menus;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AbrirCSV;
+        private System.Windows.Forms.OpenFileDialog Abrir;
+        private System.Windows.Forms.SaveFileDialog Guardar;
+        private System.Windows.Forms.ToolStripMenuItem exportarToolStripMenuItem;
+        private System.Windows.Forms.DataGridView DGV;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acerdaDeToolStripMenuItem;
+    }
+}
+
---
+++ Edmodo Csv/Edmodo Csv/Main.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+using ClosedXML.Excel;
+
+namespace Edmodo_Csv
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void AbrirCSV_Click(object sender, EventArgs e)
+        {
+            if (Abrir.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    CsvtoDatagridView(Abrir.FileName);
+                    this.Text = Abrir.SafeFileName;
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show(Ex.Message);
+                }        
+            }
+        }
+        private void exportarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Exportar();
+        }
+        public void CsvtoDatagridView(string Csv)
+        {
+            DataTable Tb = new DataTable();
+            string[] Lineas = File.ReadAllLines(Csv);
+            DataRow Row;
+            if (Lineas.Length > 0)
+            {
+                string PrimerLinea = Lineas[0];
+
+                string[] Etiquetas = PrimerLinea.Split(',');
+
+                foreach (string Encabezado in Etiquetas)
+                {
+                    Tb.Columns.Add(new DataColumn(Encabezado));
+                }
+                Tb.Columns.Add("NF");
+                for (int I = 1; I <= Lineas.Length; I++)
+                {
+                    string[] Data = Lineas[I].Split(',');
+                    int ColumnIndex = 0;
+                    Row = Tb.NewRow();
+                    string Pattern = @"[0-9 / 0-9]";
+                    Regex Re = new Regex(Pattern);
+                    List<int> Su = new List<int>();
+                    foreach (string Datah in Etiquetas)
+                    {
+                        int num = 0;
+                        if (Re.IsMatch(Data[ColumnIndex]))
+                        {
+                            Re = new Regex("[0-9]");
+
+                            if (Re.IsMatch(Data[ColumnIndex]))
+                            {
+                                num = int.Parse(Data[ColumnIndex++].Replace("\"", "").Replace("=", "").Substring(0, 2));
+                                Row[Datah] = num;
+                                Su.Add(num);
+                            }
+                            else
+                            {
+                                Row[Datah] = Data[ColumnIndex++].Replace("\"", "").Replace("=", "");
+                            }
+                        }
+                        else
+                        {
+                            Row[Datah] = Data[ColumnIndex++].Replace("\"", "").Replace("=", "");
+                        }
+                    }
+                    
+                    Row["NF"] = Su.Sum();
+                    Tb.Rows.Add(Row);
+                    DGV.DataSource = Tb;
+                }
+            }
+        }
+
+        public void OldCsvtoDatagridView(string Csv)
+        {
+            DataTable Tb = new DataTable();
+            string[] Lineas = File.ReadAllLines(Csv);
+            DataRow Row;
+            if (Lineas.Length > 0)
+            {
+                string PrimerLinea = Lineas[0];
+
+                string[] Etiquetas = PrimerLinea.Split(',');
+
+                foreach (string Encabezado in Etiquetas)
+                {
+                    Tb.Columns.Add(new DataColumn(Encabezado));
+                }
+                Tb.Columns.Add("NF");
+                for (int I = 1; I < Lineas.Length; I++)
+                {
+                    string[] Data = Lineas[I].Split(',');
+                    int ColumnIndex = 0;
+                    Row = Tb.NewRow();
+                    List<int> Su = new List<int>();
+                    foreach (string Datah in Etiquetas)
+                    {
+                        Row[Datah] = Data[ColumnIndex++];
+
+                    }
+
+                    Tb.Rows.Add(Row);
+                    DGV.DataSource = Tb;
+                }
+            }
+        }
+        public void Exportar()
+        {
+            try
+            {
+                DataTable Tb = new DataTable();
+                foreach (DataGridViewColumn Col in DGV.Columns)
+                {
+                    Tb.Columns.Add(Col.HeaderText, Col.ValueType);
+                }
+
+                foreach (DataGridViewRow row in DGV.Rows)
+                {
+                    Tb.Rows.Add();
+                    foreach (DataGridViewCell Cells in row.Cells)
+                    {
+                        Tb.Rows[Tb.Rows.Count - 1][Cells.ColumnIndex] = Cells.Value.ToString();
+                    }
+                }
+                using (XLWorkbook wb = new XLWorkbook())
+                {
+                    wb.Worksheets.Add(Tb, "Reporte");
+
+                    wb.Worksheet(1).Cells("A1:C1").Style.Fill.BackgroundColor = XLColor.DarkGreen;
+                    for (int i = 1; i <= Tb.Rows.Count; i++)
+                    {
+
+                        string cellRange = string.Format("A{0}:C{0}", i + 1);
+                        if (i % 2 != 0)
+                        {
+                            wb.Worksheet(1).Cells(cellRange).Style.Fill.BackgroundColor = XLColor.GreenYellow;
+                        }
+                        else
+                        {
+                            wb.Worksheet(1).Cells(cellRange).Style.Fill.BackgroundColor = XLColor.Yellow;
+                        }
+
+                    }
+                    wb.Worksheet(1).Columns().AdjustToContents();
+                    Guardar.FileName = Abrir.FileName;
+                    if (Guardar.ShowDialog() == DialogResult.OK)
+                    {
+                        wb.SaveAs(Guardar.FileName);
+                    }
+                }
+                SystemSounds.Exclamation.Play();
+                MessageBox.Show("Exportado Completo!!!");
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+
+        private void acerdaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Acerca Ac = new Acerca();
+            Ac.ShowDialog();
+        }
+    }
+}
